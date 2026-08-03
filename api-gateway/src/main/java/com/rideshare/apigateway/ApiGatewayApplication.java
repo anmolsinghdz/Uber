4ed891fef96f724aaebed88768a55b720f636d7d
Anmol Singh <anmolsingh0914@gmail.com>
2026-08-04 02:03:45 +0530

    Discovery Client, Config Server, and Gateway

--- api-gateway/src/main/java/com/rideshare/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/rideshare/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.rideshare.locationservice;
+package com.rideshare.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class LocationServiceApplication {
+public class ApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LocationServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
--- matching-service/src/main/java/com/rideshare/matchingservice/MatchingServiceApplication.java
+++ matching-service/src/main/java/com/rideshare/matchingservice/MatchingServiceApplication.java
@@ -2,10 +2,12 @@ package com.rideshare.matchingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class MatchingServiceApplication {
 
 	public static void main(String[] args) {
